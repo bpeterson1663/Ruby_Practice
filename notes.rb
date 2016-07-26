e048a5618b63cf2ab0847dec76a2a8014ce622f4
bpeterson1663 <bpeterson1663@gmail.com>
2016-07-26 12:46:13 +0000

    Working on refactoring Ruby code for BP

--- notes.rb
+++ notes.rb
@@ -224,5 +224,51 @@ before using ternary compairson
   end
 =end
 
-#after 
+#after
 puts 1 < 2 ? "One is less than two!" : "One is not less than two."
+
+
+
+=begin
+  Working on practicing some of Ruby shorthand hand refactoring code
+  Here is a chunk of code I was given to refactor
+          $VERBOSE = nil    # We'll explain this at the end of the lesson.
+          require 'prime'   # This is a module. We'll cover these soon!
+
+          def first_n_primes(n)
+
+            unless n.is_a? Integer
+              return "n must be an integer."
+            end
+
+            if n <= 0
+              return "n must be greater than 0."
+            end
+
+            prime_array = [] if prime_array.nil?
+
+            prime = Prime.new
+            for num in (1..n)
+              prime_array.push(prime.next)
+            end
+            return prime_array
+          end
+
+          first_n_primes(10)
+
+=end
+
+#And refactored
+require 'prime'
+
+def first_n_primes(n)
+  # Check for correct input!
+  "n must be an integer" unless n.is_a? Integer
+  "n must be greater than 0" if n <= 0
+
+  # The Ruby 1.9 Prime class makes the array automatically!
+  prime = Prime.instance
+  return prime.first n
+end
+
+first_n_primes(10)
