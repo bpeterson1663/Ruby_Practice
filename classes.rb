8d61f201104c8d3022e520e1d0035f3f8861e2aa
bpeterson1663 <bpeterson1663@gmail.com>
2016-08-04 03:21:30 +0000

    Working with inheritance and class variables

--- classes.rb
+++ classes.rb
@@ -8,3 +8,35 @@ end
 
 brady = Person.new("Brady") #creates a new person variable passing in "Brady" into the initialize method
 puts "Brady object is #{brady}"
+#working with class variables
+
+class Person
+  @@people_count = 0 #class variable
+  def initialize(name)
+    @name = name
+    @@people_count += 1
+  end
+
+  def self.number_of_instances
+    return @@people_count
+  end
+end
+
+matz = Person.new("Yukihiro")
+dhh = Person.new("David")
+
+puts "Number of Person instances: #{Person.number_of_instances}" #outputs 2
+
+#Working with inheritance
+class ApplicationError
+  def display_error
+    puts "Error! Error!"
+  end
+end
+
+class SuperBadError < ApplicationError #SuperBadError will inherit from ApplicationError #reads like SuperBadError inherits from ApplicationError
+end
+
+err = SuperBadError.new
+err.display_error
+
