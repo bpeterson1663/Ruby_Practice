2c76043a2c5a40df008413e43b09a61bafeaab5e
bpeterson1663 <bpeterson1663@gmail.com>
2016-08-05 02:43:28 +0000

    More with classes and variables

--- classes.rb
+++ classes.rb
@@ -40,3 +40,18 @@ end
 err = SuperBadError.new
 err.display_error
 
+class Message
+    @@messages_sent = 0
+   def initialize (from, to)
+       @from = from
+       @to = to
+       @@messages_sent += 1
+   end
+end
+
+class Email < Message
+    def initialize (subject)
+       @subject = subject
+    end
+end
+
